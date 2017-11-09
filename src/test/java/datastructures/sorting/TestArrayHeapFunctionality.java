026bca68dca1b49f68ec6843ec012e3ad4c34373
Armin Rouz <arminrouz@PenguinPro.domain>
2017-11-08 16:50:54 -0800

    Test Changes

--- src/test/java/datastructures/sorting/TestArrayHeapFunctionality.java
+++ src/test/java/datastructures/sorting/TestArrayHeapFunctionality.java
@@ -3,7 +3,7 @@ package datastructures.sorting;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
-
+import java.util.*; 
 import misc.BaseTest;
 import datastructures.concrete.ArrayHeap;
 import datastructures.interfaces.IPriorityQueue;
@@ -23,5 +23,71 @@ public class TestArrayHeapFunctionality extends BaseTest {
         IPriorityQueue<Integer> heap = this.makeInstance();
         heap.insert(3);
         assertEquals(1, heap.size());
+    }
+    
+    @Test(timeout=SECOND)
+    public void testExtendedSize() {
+    	IPriorityQueue<Integer> heap = this.makeInstance();
+    	Random rn = new Random();
+    	for (int i = 0; i < 750; i++) {
+    		heap.insert(rn.nextInt(2049) + 1);
+    	}
+    	assertEquals(750, heap.size());
+    }
+    
+    @Test(timeout=SECOND)
+    public void testMultipleRemoveSize() {
+    	IPriorityQueue<Integer> heap = this.makeInstance();
+    	Random rn = new Random();
+    	for (int i = 0; i < 850; i++) {
+    		heap.insert(rn.nextInt(2049) + 1);
+    	}
+    	
+    	for (int i = 0; i < 10; i++) {
+    		heap.removeMin();
+    	}
+    	assertEquals(840, heap.size());
+    }
+    
+    @Test(timeout=SECOND)
+    public void testMinPeek() {
+    	Random rn = new Random();
+    	IPriorityQueue<Integer> heap = this.makeInstance();
+    	heap.insert(-1);
+    	for (int i = 0; i < 500; i++) {
+    		heap.insert(rn.nextInt(2049) + 1);
+    	}
+    	assertEquals(heap.peekMin(), -1);
+    }
+    
+    @Test(timeout=SECOND)
+    public void testMinReturn() {
+    	Random rn = new Random();
+    	IPriorityQueue<Integer> heap = this.makeInstance();
+    	heap.insert(-2);
+    	heap.insert(-1);
+    	for (int i = 0; i < 500; i++) {
+    		heap.insert(rn.nextInt(2049) + 1);
+    	}
+    	int returnedMin = heap.removeMin();
+    	assertEquals(returnedMin, -2);
+    	assertEquals(heap.peekMin(), -1); 
+    			
+    }
+    
+    @Test(timeout=SECOND)
+    public void testInsertBasic() {
+    	IPriorityQueue<Integer> heap = this.makeInstance();
+    	heap.insert(1);
+    	assertEquals(heap.peekMin(), 1);
+    }
+    
+    public void testInsertWithChildren() {
+    	IPriorityQueue<Integer> heap = this.makeInstance();
+    	for(int i = 1; i<= 5; i++) {
+    		heap.insert(i);
+    	}
+    	heap.insert(-1);
+    	assertEquals(heap.peekMin(), -1);
     }
 }
