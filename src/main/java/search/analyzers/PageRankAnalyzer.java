f2b04e22c4f8ce59ce152149c6884181e7283611
Armin Rouz <arouz@uw.edu>
2017-11-29 00:08:24 -0800

    fuck

--- src/main/java/search/analyzers/PageRankAnalyzer.java
+++ src/main/java/search/analyzers/PageRankAnalyzer.java
@@ -100,40 +100,106 @@ public class PageRankAnalyzer {
     		initRank.put(pair.getKey(), 1.0 / (double) graph.size());
     	}
     	 // Step 2: The update step should go here
-    	IDictionary<URI, Double> finalRank = new ArrayDictionary<URI, Double>();
+    	IDictionary<URI, Double> updated = new ArrayDictionary<URI, Double>();
+    	IDictionary<URI, Double> zeros = new ArrayDictionary<URI, Double>();
 
     	for (KVPair<URI, ISet<URI>> pair : graph) {
-    		finalRank.put(pair.getKey(), 0.0);
+    		zeros.put(pair.getKey(), 0.0);
     	}
+    	updated = zeros;
     	
-        for (int i = 0; i < limit; i++) {
-        	boolean check = true;
-    		IDictionary<URI, Double> intermediate = new ArrayDictionary<URI, Double>();
-
-        	for (KVPair<URI, ISet<URI>> pair : graph) { // FOR EACH PAGE (VERTEX) IN THE GRAPH
-        		double newRank = 0;
-        		for (URI uri : pair.getValue()) { // FOR EACH THING THAT LINKS TO IT
-    				double size = (double) graph.get(uri).size(); // NUMBER IT LINKS TO
-            		if(size == 0.0) { // IF NO OUTGOING LINKS
-            			size = (double) graph.size(); // THEN USE GRAPH SIZE
-            		}
-            		if(i == 0) { // IF FIRST ITERATION, USE VALUES IN INITIAL RANK DICT
-            			newRank += decay * initRank.get(uri) / size;
-            		} else { // ELSE USE LAST UPDATED VALUE
-            			newRank += decay * finalRank.get(uri) / size;
-
-            		}
+    	
+    	IDictionary<URI, Double> intermediate;
+    	for (int i = 0; i < limit; i++) {
+    		intermediate = zeros;
+    		for (KVPair<URI, ISet<URI>> pair : graph) {
+    			URI uri = pair.getKey();
+    			ISet<URI> linksTo = pair.getValue();
+    			if (linksTo.isEmpty()) {
+    				for(KVPair<URI, Double> pair2 : intermediate) {
+    					URI nextUri = pair2.getKey();
+    					Double oldVal = pair2.getValue();
+    					intermediate.put(nextUri, oldVal + (decay * oldVal / (double) graph.size()));
+    				}
     			}
-    			newRank += (1.0 - decay) / (double) graph.size(); // DECAY FACTOR
-        		if(check == true && Math.abs(finalRank.get(pair.getKey()) - newRank) > epsilon) {
-        			check = false;
-        		}
-        		intermediate.put(pair.getKey(), newRank);
-        	}
-        	finalRank = intermediate;
-        	if(check == true) {
-        		return finalRank;
-        	}
+    			for (URI link : linksTo) {
+    				double val;
+    				if(i == 0) {
+    					val = initRank.get(link);
+    				} else {
+    					val = updated.get(link);
+    				}
+    				if(graph.get(link).size() > 0) {
+    					intermediate.put(uri, intermediate.get(uri) + (decay * val / (double) graph.get(link).size()));
+    				}
+    			}
+    			for (KVPair<URI, Double> pair3 : intermediate) {
+    				double oldValue = pair3.getValue();
+    				double newVal = oldValue + ((1 - decay) / (double) graph.size());
+    				intermediate.put(pair3.getKey(), newVal);
+    			}
+    		}
+    		boolean check = true;
+    		for (KVPair<URI, Double> finalPair : updated) {
+    			URI uri2 = finalPair.getKey();
+    			if(check == true && Math.abs(updated.get(uri2) - intermediate.get(uri2)) >= epsilon) {
+    				check = false;
+    			}
+    		}
+    		updated = intermediate;
+    		if (check == true) {
+    			return updated;
+    		}
+    	}
+//    	
+//        for (int i = 0; i < limit; i++) {
+//        	boolean check = true;
+//    		IDictionary<URI, Double> intermediate = new ArrayDictionary<URI, Double>();
+//    		
+//    		int counter = 0;
+//        	for (KVPair<URI, ISet<URI>> pair : graph) { // FOR EACH PAGE (VERTEX) IN THE GRAPH
+//        		//int counter = 0;
+//        		double newRank = 0;
+//        		if(pair.getValue().isEmpty()) {
+//        			for(KVPair<URI, Double> pair2 : finalRank) {
+//        				URI uri = pair2.getKey();
+//        				if(i == 0) {
+//        					newRank = initRank.get(uri);
+//        					
+//        				} else {
+//        					newRank = finalRank.get(uri);
+//        				}
+//        				//System.out.println(newRank);
+//        				newRank +=  (decay * newRank) / (double) graph.size();
+//    					//intermediate.put(uri, newRank + (decay * newRank) / (double) graph.size());
+//
+//        			}
+//        		}
+//        		for (URI uri : pair.getValue()) { // FOR EACH THING THAT LINKS TO IT
+//    				double size = (double) graph.get(uri).size(); // NUMBER IT LINKS TO
+//            		if(size != 0.0) { // IF NO OUTGOING LINKS
+//            		
+//	            		if(i == 0) { // IF FIRST ITERATION, USE VALUES IN INITIAL RANK DICT
+//	            			newRank += decay * initRank.get(uri) / size;
+//	            		} else { // ELSE USE LAST UPDATED VALUE
+//	            			newRank += decay * finalRank.get(uri) / size;
+//	            		}
+//            		}
+//    			}
+//    			newRank += (1.0 - decay) / (double) graph.size(); // DECAY FACTOR
+//        		if(/*check == true &&*/ Math.abs(finalRank.get(pair.getKey()) - newRank) >= epsilon) {
+//        			//check = false;
+//        			counter++;
+//        		}
+//        		
+//        		//System.out.println(newRank);
+//    
+//        		intermediate.put(pair.getKey(), newRank);
+//        	}
+//        	finalRank = intermediate;
+//        	if(/*check == true*/ counter == 0) {
+//        		return finalRank;
+//        	}
         	// ADD A CHECK FOR IF ONE OF THE WEBPAGES DOESNT HAVE OUTGOING LINKS, DIVIDE BY N
         	// INSTEAD OF NUMBER OF OUTGOING LINKS.
         	
@@ -172,9 +238,9 @@ public class PageRankAnalyzer {
 //        		return finalRank;
 //        	} 
 //            
-        }
+//        }
 //        return finalRank;
-    return finalRank;
+    return updated;
     }
 
     /**
